333240721283470b53c70fd2b4bb6ae01863a9ec
camelot1399 <camelot1399@mail.ru>
2021-05-01 21:07:39 +0300

    add about

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -14,7 +14,7 @@ class NewsController extends Controller
     }
     public function index()
     {
-        return view('news');
+        return view('News/news');
     }
 
     public function newsId()
