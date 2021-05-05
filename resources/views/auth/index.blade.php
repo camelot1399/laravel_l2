a2d5b1f88362e89bfbaa577cc5d3a77e77be72ea
camelot1399 <camelot1399@mail.ru>
2021-05-05 11:06:37 +0300

    ux ui

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -6,39 +6,49 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Auth</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<header>
-    <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/news">News</a></li>
-            <li><a href="/categories">Categories list</a></li>
-            <li><a href="/auth">Auth</a></li>
-        </ul>
-    </nav>
-</header>
+    <header class="wrapper">
+        <a href="/">Logo</a>
 
-    <h1>Auth</h1>
+        <nav>
+            <ul class="navHeader__ul">
+                <li class="navHeader__li"><a class="navHeader__a" href="/">Home</a></li>
+                <li class="navHeader__li"><a class="navHeader__a" href="/about">About</a></li>
+                <li class="navHeader__li"><a class="navHeader__a" href="/news">News</a></li>
+                <li class="navHeader__li"><a class="navHeader__a" href="/categories">Categories list</a></li>
+                <li class="navHeader__li"><a class="navHeader__a" href="/auth">Auth</a></li>
+                <li class="navHeader__li"><a class="navHeader__a" href="/adm">Adminka</a></li>
+            </ul>
+        </nav>
+    </header>
 
-    <form action="/adm">
-        <label for="login">
-            Login:
-            <input type="text" name="login" placeholder="login" id="login" required>
-        </label>
-        <br>
-        <label for="password">
-            Password:
-            <input type="password" name="password" placeholder="password" id="password" required>
-        </label>
-        <br>
-        <label for="saveMe">
-            Запомнить меня:
-            <input type="checkbox" name="saveMe" id="saveMe">
-        </label>
-        <br>
-        <button type="submit">Войти</button>
-    </form>
+    <main class="wrapper">
+        <h1>Auth</h1>
+
+        <form action="/adm">
+            <label for="login">
+                Login:
+                <input type="text" name="login" placeholder="login" id="login" required>
+            </label>
+            <br>
+            <label for="password">
+                Password:
+                <input type="password" name="password" placeholder="password" id="password" required>
+            </label>
+            <br>
+            <label for="saveMe">
+                Запомнить меня:
+                <input type="checkbox" name="saveMe" id="saveMe">
+            </label>
+            <br>
+            <button type="submit">Войти</button>
+        </form>
+    </main>
+
+    <footer class="wrapper">
+        footer
+    </footer>
 </body>
 </html>
