ce9836af70a20c4d168e367075190f03b07b6d38
camelot1399 <camelot1399@mail.ru>
2021-05-06 11:23:51 +0300

    up

--- resources/views/components/news/list.blade.php
+++ resources/views/components/news/list.blade.php
@@ -1,12 +1,17 @@
-<div class="border-2">
+<div class="border-2 mb-2 p-2">
     <a href="{{ route('news.show', ['news' => $newsItem]) }}">
-        <p>{{ $newsItem->category->name }}</p>
-        <div>
+
+        <div class="font-bold">
             {{ $newsItem->title }}}
         </div>
 
-        <div class="">
-            {{ $newsItem->created_at->format('d.m.Y') }}
+        <div class="flex font-small mt-5">
+            <div>
+                {{ $newsItem->category->name }}
+            </div>
+            <div class="">
+                {{ $newsItem->created_at->format('d.m.Y') }}
+            </div>
         </div>
     </a>
 </div>
